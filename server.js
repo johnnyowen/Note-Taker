5bedab42a0f5480234ad31c2f67af8f4e583d8c1
John Owen <j.owen1993@outlook.com>
2023-05-30 21:59:26 -0700

    further along, almost done, just have to add delete function

--- server.js
+++ server.js
@@ -11,30 +11,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-// app.get('/', (req, res) =>
-//   res.sendFile(path.join(__dirname, '/public/index.html'))
-// );
-
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
+  );
+  
 app.get('/api/notes', (req, res) => 
-    // res.json(db)
-    fs.readFile('./db/db.json', (err, data) => {
-      if (err) throw err;
-      let allNotes = JSON.parse(data);
-      res.json(allNotes)
-    })
+fs.readFile('./db/db.json', (err, data) => {
+  if (err) throw err;
+  let allNotes = JSON.parse(data);
+  res.json(allNotes)
+})
 );
 
 app.post('/api/notes', (req, res) => {
-    const newNote = req.body;
-    newNote.id = uuidv4();
-    db.push(newNote)
-    fs.writeFileSync('./db/db.json', JSON.stringify(db))
-    res.json(db)
-  }
+  const newNote = req.body;
+  newNote.id = uuidv4();
+  db.push(newNote)
+  fs.writeFileSync('./db/db.json', JSON.stringify(db))
+  res.json(db)
+}
 );
 
 app.delete('/api/reviews/:id', (req, res) => {
@@ -47,6 +42,10 @@ app.delete('/api/reviews/:id', (req, res) => {
   })
 })
 
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 app.listen(PORT, () => {
-    console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}`)
 });
