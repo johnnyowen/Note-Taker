06cfd1582bdef99cc81803178f341562c0ad702a
johnnyowen <j.owen1993@outlook.com>
2023-06-01 21:41:09 -0700

    heroku push

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ app.get('*', (req, res) =>
 
 // Tells the app where to listen
 app.listen(PORT, () => {
-  console.log(`App listening at http://localhost:${PORT}`)
+  console.log(`App listening at http://localhost:${PORT}🚀`)
 });
